84c5b6526249eca54435604faae463597348ebca
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-05-09 20:20:33 +0530

    css changes

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -29,7 +29,6 @@ function CreatePost({ isAuth }) {
   return (
     <div className="createPostPage">
       <div className="cpContainer">
-        <h1>Create A Post</h1>
         <div className="inputGp">
           <label> Title:</label>
           <input
